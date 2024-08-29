1625a558a1e139de1f1df49243550153539298cb
jerehirvonenn <jerehirvonen21@gmail.com>
2024-08-29 11:38:30 +0300

    starting and initializing threads

--- includes/philo.h
+++ includes/philo.h
@@ -3,4 +3,29 @@
 
 #include <stdlib.h>
 
+typedef struct s_rules
+{
+	int			philo_num;
+	int			time_die;
+	int			time_eat;
+	int			time_sleep;
+	int			num_eat;
+	int			all_full;
+	int			dead;
+	t_philo			*philo;
+	p_thread _mutex_t	*forks;
+}	t_rules;
+
+typedef struct s_philo
+{
+	int		id;
+	int		meals;
+	t_mutex		*left_fork;
+	t_mutex		*right_fork;
+	t_rules		*data;
+}	t_philo;
+
+int	check_args(int ac, char **av, t_rules *data);
+int	ft_atoi(const char *str, int *res);
+
 #endif
--- src/philo.c
+++ src/philo.c
@@ -1,5 +1,98 @@
 #include "../includes/philo.h"
+#include <stdio.h> //DELETE
 
-int main(int ac, char **av)
+int	ft_atoi(const char *str, int *res)
 {
+	long long int	result;
+	int				i;
+
+	i = 0;
+	result = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+		if (str[i++] == '-')
+			return (1);
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i++] - '0');
+		if (result > 2147483648)
+			return (1);
+	}
+	if (str[i] || result <= 0)
+		return (1);
+	*res = result;
+	return (0);
+}
+
+int	check_args(int ac, char **av, t_rules *data)
+{
+	if (ac > 6 || ac < 5)
+		return (1);
+	if (ft_atoi(av[1], &data->philo_num))
+		return(1);
+	if (ft_atoi(av[2], &data->time_die))
+		return(1);
+	if (ft_atoi(av[3], &data->time_eat))
+		return(1);
+	if (ft_atoi(av[4], &data->time_sleep))
+		return(1);
+	if (ac == 6 && ft_atoi(av[5], &data->num_eat))
+		return(1);
+	if (ac == 5)
+		data->num_eat = -1;
+	data->all_full = 0;
+	data->dead = 0;
+	return (0);
+}
+
+void	test_rules(t_rules *data)
+{
+	printf("Number of philos:%d\n", data->philo_num);
+	printf("Time to die     :%d\n", data->time_die);
+	printf("Time to eat     :%d\n", data->time_eat);
+	printf("Time to sleep   :%d\n", data->time_sleep);
+	printf("Number of meals :%d\n", data->num_eat);
+}
+
+void	philo_and_lock(t_rules *data)
+{
+	int i;
+
+	i = -1;
+	while (++i < data->philo_num)
+	{
+		data->philo[i].id = i + 1;
+		data->philo[i].meals = 0;
+		data->philo[i].leftfork = i;
+		data->philo[i].rightfork = (i + 1) & data->philonum;
+	}
+}
+
+int	init_philo(t_rules *data)
+{
+	int i;
+
+	i = 0;
+	data->philo = malloc(sizeof(t_philo) * data->philo_num);
+	if (!data->philo)
+		return (1);
+	data->forks = malloc(sizeof(pthread_mutex_t) * data->philo_num);
+	if (!data->forks)
+	{
+		free(data->philo);
+		return (1);
+	}
+	philo_and_lock(data);
+}
+
+int	main(int ac, char **av)
+{
+	t_rules data;
+
+	if (check_args(ac, av, &data))
+		return (1);
+	test_rules(&data); //testing
+	if (init_philo(&data))
+		return (1);
 }
